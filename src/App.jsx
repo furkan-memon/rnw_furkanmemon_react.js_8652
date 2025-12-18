7ab1bd6b0e1cce62382f2c95db34d3fc8fc7970c
furkan-memon <furkanmemon860@gmai.com>
2025-12-18 15:10:31 +0530

    18-20

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card'
+const App = () => {
+  return (
+    <>
+    <Card user='amaan' img='https://images.unsplash.com/photo-1764893216236-67a9f22aa0a4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+   <Card user='Montu' img='https://images.unsplash.com/photo-1764970692776-ce5fb30a7509?q=80&w=1382&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+    </>
+  )
+}
+
+export default App
