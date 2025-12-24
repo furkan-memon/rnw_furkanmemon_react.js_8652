c3ae87b161782271797f92239df1e07877b979f4
furkan_memon <furkanmemon860@gmail.com>
2025-12-24 08:06:26 +0000

    12-24

---
+++ 03-Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-100 min-h-screen flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-blue-600'>Hello, Tailwind CSS with Vite and React!</h1>
+    </div>
+  )
+}
+
+export default App
